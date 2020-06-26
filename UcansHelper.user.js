205f1c889955c9f13490842802baf65d0232d3ee
MyBlueHorizon <tianjizhishang@outlook.com>
2020-06-26 08:00:11 +0800

    Update UcansHelper.user.js

--- UcansHelper.user.js
+++ UcansHelper.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         空中课堂 辅助脚本
 // @namespace    UcansHelper
-// @version      1.0.1
+// @version      1.0.2
 // @description  自动签到
-// @author       Nobody
+// @author       MyBlueHorizon
 // @match        *://www.ucans.net/chatRoom/*
 // @grant        none
 // @icon         http://www.ucans.net/chatRoom/favicon.ico
@@ -15,20 +15,17 @@
     setInterval(clickBtn, 3000)
     // Your code here...
 })();
+
 function clickBtn() {
     'use strict';
     let btn_elements = document.getElementsByClassName("st_Roll");
     var getcheck = document.getElementsByClassName("st_Roll").rollDiv.children;
     var myDate = new Date();
     var mytime=myDate.toLocaleTimeString();
-    console.log("探测签到中")
     try {
         if (btn_elements.rollDiv.style.display == "" || btn_elements.rollDiv.style.display =="block") {
-            console.info("签到已成功"+ mytime)
+            console.info("签到已成功，于"+ mytime)
             getcheck[1].click()
-        }
-        if (btn_elements.rollDiv.style.display == "none") {
-            console.log("当前无签到")
         }
     } catch(err) {
         console.error(`脚本错误:${err}`)
