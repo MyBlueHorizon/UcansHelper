0665195a9c6888135303ec0c98e259914a9dd7f2
TX Zhang <2679363155@qq.com>
2020-06-27 09:19:37 +0800

    1.0.3

--- UcansHelper.user.js
+++ UcansHelper.user.js
@@ -1,16 +1,17 @@
 // ==UserScript==
-// @name         空中课堂 辅助脚本
+// @name         空中课堂（友看名师云课堂）辅助脚本
 // @namespace    UcansHelper
-// @version      1.0.2
+// @version      1.0.3
 // @description  自动签到
 // @author       MyBlueHorizon
+// @supportURL   https://github.com/MyBlueHorizon/UcansHelper/issues
 // @match        *://www.ucans.net/chatRoom/*
 // @grant        none
 // @icon         http://www.ucans.net/chatRoom/favicon.ico
 // @license      MIT License
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
     setInterval(clickBtn, 3000)
     console.info("脚本已启用")
@@ -23,11 +24,11 @@ function clickBtn() {
     var myDate = new Date();
     var mytime = myDate.toLocaleTimeString();
     try {
-        if (btn_elements.rollDiv.style.display == "" || btn_elements.rollDiv.style.display =="block") {
+        if (btn_elements.rollDiv.style.display == "" || btn_elements.rollDiv.style.display == "block") {
             getcheck[1].click()
-            console.info("签到已成功，于"+ mytime)
+            console.info("签到已成功，于" + mytime)
         }
-    } catch(err) {
+    } catch (err) {
         console.error(`脚本错误:${err}`)
     }
 }
