68ab50906f579ce7a2052706ad17cc33a8c6e866
MyBlueHorizon <tianjizhishang@outlook.com>
2021-06-06 11:56:54 +0800

    Update UcansHelper.user.js

--- UcansHelper.user.js
+++ UcansHelper.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         友看课堂小助手
 // @namespace    UcansHelper
-// @version      1.3.6
+// @version      1.4.0
 // @description  [非官方] 云课堂自动签到等辅助功能及优化。
 // @author       MyBlueHorizon
 // @supportURL   https://github.com/MyBlueHorizon/UcansHelper/issues
 // @match        *://www.ucans.net/chatRoom/*
 // @require      https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js
 // @require      https://cdn.jsdelivr.net/npm/toastr@2.1.4/toastr.min.js
-// @resource     toastrcss https://cdn.jsdelivr.net/npm/toastr@2.1.4/build/toastr.min.css
+// @resource     toastrCSS https://cdn.jsdelivr.net/npm/toastr@2.1.4/build/toastr.min.css
 // @grant        GM_getValue
 // @grant        GM_addStyle
 // @grant        GM_setValue
@@ -16,110 +16,161 @@
 // @icon         https://www.ucans.net/chatRoom/favicon.ico
 // @license      MIT License
 // ==/UserScript==
-var InitInterval
+
+var danmakuInitInterval
+
 (function() {
+
     'use strict';
-    var NowUrl = window.location.pathname;
-    GM_addStyle(GM_getResourceText("toastrcss"))
+
+    //添加 toastr 通知样式表
+    GM_addStyle(GM_getResourceText("toastrCSS"))
+
+    //判断浏览器是否支持通知 API
     if (!("Notification" in window)) {
+
         toastr.warn("浏览器不支持桌面通知，部分功能无法使用，请更新浏览器")
+
     }
-    if (NowUrl == "/chatRoom/video_live.html"){
-        if(GM_getValue("AutoCheckRoll", true)==true){
+
+    //判断当前页面
+    var nowUrl = window.location.pathname;
+
+    //直播页面
+    if (nowUrl == "/chatRoom/video_live.html"){
+
+        //判断是否开启签到
+        if(GM_getValue("isAutoCheckRoll", true)==true){
             setInterval(ClickBtn, 3000)
         }
-        if(GM_getValue("DanmakuDiscuss", true)==true){
-        InitInterval=setInterval(readyinit,1000)
+
+        //判断消息是否弹幕化
+        if(GM_getValue("isDanmakuDiscuss", true)==true){
+            danmakuInitInterval=setInterval(readyDanmakuInit,1000)
         }
-        console.info("脚本已启用")
+
+        console.info("UcansHelper 载入成功")
         toastr.info("脚本已启用")
-        toastr.warn("请合理使用，脚本造成的一切后果概不负责!")
+        toastr.warning("请合理使用，脚本造成的一切后果概不负责!","免责声明")
+
     }
-    if (NowUrl == "/chatRoom/Course_page.html"){
-        console.info("脚本已启用")
-        if(GM_getValue("ShowClassTime", true)==true){
-        toastr.info(localStorage.getItem("localStudyingTimeLive"),"听课时间")
+
+    //课程页面
+    if (nowUrl == "/chatRoom/Course_page.html"){
+
+        console.info("UcansHelper 载入成功")
+
+        //显示听课时间
+        if(GM_getValue("isShowClassTime", true)==true){
+
+            toastr.info(localStorage.getItem("localStudyingTimeLive"),"听课时间")
+
         }
-        ChangeCoursePage()
-        //document.getElementsByClassName("svg-box")[0].style.marginTop=document.documentElement.clientHeight-document.getElementById("svgbox").offsetTop+"px"
-        //document.getElementsByClassName("svg-box")[0].style.marginTop=
-        document.getElementsByClassName("svg-box")[0].style.fontSize="medium"
-        //document.getElementsByClassName("svg-box2")[0].style.marginBottom=40
-        document.getElementsByClassName("svg-box2")[0].style.fontSize="medium"
+
+        //修改课程页面
+        changeCoursePage()
+
+        //绑定设置按钮实践
         let settingButton = document.getElementById('settingButton');
         settingButton.onclick = showSettingBox;
-        document.getElementById("autoCheckRoll").checked=GM_getValue("AutoCheckRoll", true)
+
+        //载入设置状态
+
+        //自动签到
+        document.getElementById("autoCheckRoll").checked=GM_getValue("isAutoCheckRoll", true)
         let autoCheckRoll = document.getElementById('autoCheckRoll');
         autoCheckRoll.onclick = setAutoCheckRoll;
-        document.getElementById("showClassTime").checked=GM_getValue("ShowClassTime", true)
+
+        //显示上课时间
+        document.getElementById("showClassTime").checked=GM_getValue("isShowClassTime", true)
         let showClassTime = document.getElementById('showClassTime');
         showClassTime.onclick = setShowClassTime;
+
+        //重写课程页面
         let reWriteClassPage = document.getElementById('reWriteClassPage');
         reWriteClassPage.disabled=true
         reWriteClassPage.checked=true
-        document.getElementById("danmakuDiscuss").checked=GM_getValue("DanmakuDiscuss", true)
+
+        //弹幕化讨论区
+        document.getElementById("danmakuDiscuss").checked=GM_getValue("isDanmakuDiscuss", true)
         let danmakuDiscuss = document.getElementById('danmakuDiscuss');
         danmakuDiscuss.onclick = setDanmakuDiscuss;
+
+        //设定页脚动画
+        document.getElementsByClassName("svg-box")[0].style.fontSize="medium"
+        document.getElementsByClassName("svg-box")[0].style.marginBottom="0"
+
+        //页脚置底（未完成）
+        //document.getElementsByClassName("svg-box")[0].style.marginTop=document.documentElement.clientHeight-document.getElementById("svgbox").offsetTop+"px"
+        //document.getElementsByClassName("svg-box")[0].style.marginTop=
+        //document.getElementsByClassName("svg-box2")[0].style.fontSize="medium"
+
     }
+
 })();
-/*    $(function(){
-        function footerPosition(){
-            toastr.info("ss")
-            $("footer").removeClass("fixed-bottom");
-            var contentHeight = document.body.scrollHeight,//网页正文全文高度
-                winHeight = window.innerHeight;//可视窗口高度，不包括浏览器顶部工具栏
-            if(!(contentHeight > winHeight)){
-                //当网页正文高度小于可视窗口高度时，为footer添加类fixed-bottom
-                $("footer").addClass("fixed-bottom");
-            } else {
-                $("footer").removeClass("fixed-bottom");
-            }
-        }
-        footerPosition();
-        $(window).resize(footerPosition);
-    }); */
-//$(window).resize(function() {document.getElementById("svgbox").style.marginTop=document.documentElement.clientHeight-document.getElementById("svgbox").offsetTop+"px"})
 
-
-function setAutoCheckRoll(e) {
-    if (document.getElementById("autoCheckRoll").checked==true){
-        GM_setValue("AutoCheckRoll", true)
-    }
-    else {
-        GM_setValue("AutoCheckRoll", false)
-    }
-  }
-function setShowClassTime(e) {
-    if (document.getElementById("showClassTime").checked==true){
-        GM_setValue("ShowClassTime", true)
-    }
-    else {
-        GM_setValue("ShowClassTime", false)
-    }
-  }
-function setDanmakuDiscuss(e) {
-    if (document.getElementById("danmakuDiscuss").checked==true){
-        GM_setValue("DanmakuDiscuss", true)
-    }
-    else {
-        GM_setValue("DanmakuDiscuss", false)
-    }
-  }
+//显示设置页面
 function showSettingBox(e) {
+
     if (document.getElementById("setting-box").style.display=="block"){
         document.getElementById("setting-box").style.display="none"
     }
     else {
         document.getElementById("setting-box").style.display="block"
     }
+
 }
 
-function readyinit(){
+//设置更改
+
+//自动签到
+function setAutoCheckRoll(e) {
+
+    if (document.getElementById("autoCheckRoll").checked==true){
+        GM_setValue("isAutoCheckRoll", true)
+    }
+    else {
+        GM_setValue("isAutoCheckRoll", false)
+    }
+
+}
+
+//显示听课时间
+function setShowClassTime(e) {
+
+    if (document.getElementById("showClassTime").checked==true){
+        GM_setValue("isShowClassTime", true)
+    }
+    else {
+        GM_setValue("isShowClassTime", false)
+    }
+
+}
+
+//弹幕化讨论区
+function setDanmakuDiscuss(e) {
+
+    if (document.getElementById("danmakuDiscuss").checked==true){
+        GM_setValue("isDanmakuDiscuss", true)
+    }
+    else {
+        GM_setValue("isDanmakuDiscuss", false)
+    }
+
+}
+
+//弹幕相关
+
+//准备初始化弹幕
+function readyDanmakuInit(){
+
+    //判断播放器是否已加载
     if (document.getElementsByClassName("vcp-player vcp-playing").length>0) {
+
+        //注入弹幕相关元素，初始化弹幕
         var playerel=document.getElementsByClassName("vcp-player vcp-playing")
         var damakuwrapper=document.createElement("div");
         var damakustyle=document.createElement("style");
-        damakustyle.type="text/css"
         damakustyle.innerHTML="#wrapper{position:absolute;pointer-events:none;overflow:hidden;color:#FFFFFF;font-size:28px;text-shadow:2px 2px #000;left:0px;top:0px}.right{position:absolute;visibility:hidden;white-space:nowrap;transform:translateX(700px)}.left{position:absolute;white-space:nowrap;user-select:none;transition:transform 7s linear}"
         document.getElementsByTagName('head')[0].appendChild(damakustyle);
         damakuwrapper.setAttribute('id','wrapper')
@@ -129,6 +180,7 @@ function readyinit(){
         damakuwrapper.style.height=playerel[0].style.height
         playerel[0].appendChild(damakuwrapper)
         init();
+
         // 每隔1ms从弹幕池里获取弹幕（如果有的话）并发射
         setInterval(() => {
             let channel;
@@ -138,9 +190,13 @@ function readyinit(){
                 shootDanmu(dom, danmu, channel);
             }
         }, 1);
-        setInterval(AddNewMsg, 1000)
-        clearInterval(InitInterval)
+
+        //添加消息监听，完成初始化
+        setInterval(addNewMsg, 1000)
+        clearInterval(danmakuInitInterval)
+
     }
+
 }
 
 /**
@@ -210,7 +266,6 @@ function shootDanmu(dom, text, channel) {
     dom.innerText = text;
     // 如果为每个弹幕设置 transition 可以保证每个弹幕的速度相同 这里没有保证速度相同
     // dom.style.transition = `transform ${7 + dom.clientWidth / 100}s linear`;
-
     // dom.style.left = '-' + dom.clientWidth + 'px';
     // 设置弹幕的位置信息 性能优化 left -> transform
     dom.style.transform = `translateX(${-dom.clientWidth}px)`;
@@ -223,19 +278,23 @@ function shootDanmu(dom, text, channel) {
     }, dom.clientWidth * 10 + 1000);
 }
 
-var LeastTimeMsgCount=0
-function AddNewMsg(){
-    var MsgList=document.getElementsByClassName("video-sms-list")
-    var MsgCount=MsgList[0].children.length-1
-    var AlreadyMsgCount
-    for (AlreadyMsgCount=LeastTimeMsgCount;AlreadyMsgCount< MsgCount; AlreadyMsgCount=AlreadyMsgCount+1) {
-        var DanmakuText=MsgList[0].children[AlreadyMsgCount+1].children[0].children[0].children[1].innerText
-        LeastTimeMsgCount=LeastTimeMsgCount+1
-        danmuPool.push(DanmakuText)
+//监听讨论区添加弹幕
+var leastTimeMsgCount=0
+function addNewMsg(){
+    var msgList=document.getElementsByClassName("video-sms-list")
+    var msgCount=msgList[0].children.length-1
+    var alreadyMsgCount
+    for (alreadyMsgCount=leastTimeMsgCount;alreadyMsgCount< msgCount; alreadyMsgCount=alreadyMsgCount+1) {
+        var danmakuText=msgList[0].children[alreadyMsgCount+1].children[0].children[0].children[1].innerText
+        leastTimeMsgCount=leastTimeMsgCount+1
+        danmuPool.push(danmakuText)
     }
 }
 
-function ChangeCoursePage(){
+//修改课程页面
+function changeCoursePage(){
+
+    //修改元素样式
     document.getElementsByClassName("c-top-con clearfix")[0].style.width ="auto"
     document.getElementsByClassName("c-main")[0].style.width ="auto"
     document.getElementsByClassName("c-top")[0].style.background ="#1677b3"
@@ -249,33 +308,41 @@ function ChangeCoursePage(){
     document.getElementById("demand_list").style.borderBottomColor ="#1677b3"
     document.getElementById("file_list").style.color ="#1677b3"
     document.getElementById("file_list").style.borderBottomColor ="#1677b3"
+
+    //添加页面样式
+    var setstyle = document.createElement("style");
+    setstyle.innerHTML=".checke{float:right;position:relative;-webkit-appearance:none;width:40px;height:20px;line-height:20px;background:#eee;border-radius:10px;outline:none;border:2px solid #999}.checke:before{position:absolute;left:0;content:'';width:12px;height:12px;border-radius:50%;background:#eee;box-shadow:0 0 5px #ddd;transition:all .2s linear;border:2px solid #999}.checke:checked{background:#01a1d6}.checke:checked:before{left:20px;transition:all .2s linear };svg:not(:root) {overflow: hidden;}svg {width: 100%}.svg-box{height: 146px;margin-top: 100px;}.editorial {height: 150px;margin-top: -150px;}.editorial {display: block;width: 100%;height: 10em;max-height: 100vh;margin-top: -101px;}.parallax>use {animation: move-forever 12s linear infinite;}.parallax>use:nth-child(1) {animation-delay: -2s;}.parallax>use:nth-child(2) {animation-delay: -2s;animation-duration: 5s;}.parallax>use:nth-child(3) {animation-delay: -4s;animation-duration: 3s;}@keyframes move-forever {0% {transform: translate(-90px,0);}100% {transform: translate(85px,0);}"
+    document.getElementsByTagName('head')[0].appendChild(setstyle);
+
+    //添加设置按钮和内容
     var settingButton=document.createElement("li");
     var topBar=document.getElementsByClassName("c-main-btn clearfix")
     settingButton.id="settingButton"
     settingButton.style="color: rgb(22, 97, 171);float: right;margin-right: 47px;"
     settingButton.innerHTML="脚本设置";
     topBar[0].appendChild(settingButton);
-    var setstyle = document.createElement("style");
-    setstyle.type="text/css"
-    setstyle.innerHTML=".checke{float:right;position:relative;-webkit-appearance:none;width:40px;height:20px;line-height:20px;background:#eee;border-radius:10px;outline:none;border:2px solid #999}.checke:before{position:absolute;left:0;content:'';width:12px;height:12px;border-radius:50%;background:#eee;box-shadow:0 0 5px #ddd;transition:all .2s linear;border:2px solid #999}.checke:checked{background:#01a1d6}.checke:checked:before{left:20px;transition:all .2s linear };svg:not(:root) {overflow: hidden;}svg {width: 100%}.svg-box{height: 146px;margin-top: 100px;}.editorial {height: 150px;margin-top: -150px;}.editorial {display: block;width: 100%;height: 10em;max-height: 100vh;margin-top: -101px;}.parallax>use {animation: move-forever 12s linear infinite;}.parallax>use:nth-child(1) {animation-delay: -2s;}.parallax>use:nth-child(2) {animation-delay: -2s;animation-duration: 5s;}.parallax>use:nth-child(3) {animation-delay: -4s;animation-duration: 3s;}@keyframes move-forever {0% {transform: translate(-90px,0);}100% {transform: translate(85px,0);}"
-    document.getElementsByTagName('head')[0].appendChild(setstyle);
-    var donghua=document.createElement("footer");
-    donghua.className="svg-box"
-    donghua.id="svgbox"
-    document.getElementsByTagName('body')[0].appendChild(donghua)
-    document.getElementsByClassName("svg-box")[0].innerHTML="<svg class='editorial'xmlns='http://www.w3.org/2000/svg'xmlns:xlink='http://www.w3.org/1999/xlink'viewBox='0 24 150 28'preserveAspectRatio='none'><defs><path id='gentle-wave'd='M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z'></path></defs><g class='parallax'><use xlink:href='#gentle-wave'x='50'y='0'fill='rgba(128,215,175,.5)'></use><use xlink:href='#gentle-wave'x='50'y='3'fill='rgba(128,215,175,.5)'></use><use xlink:href='#gentle-wave'x='50'y='6'fill='rgba(128,215,175,.5)'></use></g></svg>"
-    var donghua2=document.createElement("footer");
-    donghua2.className="svg-box2"
-    donghua2.id="svgbox2"
-    document.getElementsByTagName('body')[0].appendChild(donghua2)
-    document.getElementsByClassName("svg-box2")[0].innerHTML="<svg class='editorial'xmlns='http://www.w3.org/2000/svg'xmlns:xlink='http://www.w3.org/1999/xlink'viewBox='0 24 150 28'preserveAspectRatio='none'><defs><path id='gentle-wave'd='M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z'></path></defs><g class='parallax'><use xlink:href='#gentle-wave'x='50'y='0'fill='rgba(128,215,175,.5)'></use><use xlink:href='#gentle-wave'x='50'y='3'fill='rgba(128,215,175,.5)'></use><use xlink:href='#gentle-wave'x='50'y='6'fill='rgba(128,215,175,.5)'></use></g></svg>"
-    document.getElementsByClassName("svg-box2")[0].style.transform="rotate(180deg)"
+
     var settingbox=document.createElement("div");
     settingbox.id="setting-box"
     settingbox.style="display: none;margin-left: 47px;margin-right: 47px;font-size: 24px;margin-bottom: 47px;background: #f8f8f8;height: 214px;"
     settingbox.innerHTML='<li style="margin-left: 24px;padding-top: 8px;">友看课堂小助手 设置</li><div style="padding : 8px 48px 0px 48px;clear : both;background: #f8f8f8; height: 24px;"><span style="float : left;display : inline-block;color : rgb(0,0,0);font-size: 18px;">自动签到</span><input type="checkbox" class="checke" id="autoCheckRoll"></div><div style="padding : 8px 48px 0px 48px;clear : both;background: #f8f8f8; height: 24px;"><span style="float : left;display : inline-block;color : rgb(0,0,0);font-size: 18px;">显示上课时间</span><input id="showClassTime" type="checkbox" class="checke"></div><div style="padding : 8px 48px 0px 48px;clear : both;background: #f8f8f8; height: 24px;"><span style="float : left;display : inline-block;color : rgb(0,0,0);font-size: 18px;">优化课程页面 [锁定]</span><input id="reWriteClassPage" type="checkbox" class="checke"></div><div style="padding : 8px 48px 0px 48px;clear : both;background: #f8f8f8; height: 24px;"><span style="float : left;display : inline-block;color : rgb(0,0,0);font-size: 18px;">评论区消息弹幕显示</span><input id="danmakuDiscuss" type="checkbox" class="checke"></div>'
     document.getElementsByClassName("c-main")[0].insertBefore(settingbox,document.getElementById("c-main-box1"));
-    document.getElementById("svgbox").style.margin="0px 0px "+document.documentElement.clientHeight-document.getElementById("svgbox").offsetTop+"px 0px"
+
+    //添加页脚
+    var donghua=document.createElement("footer");
+    donghua.className="svg-box"
+    donghua.id="svgbox"
+    document.getElementsByTagName('body')[0].appendChild(donghua)
+    document.getElementsByClassName("svg-box")[0].innerHTML="<svg class='editorial'xmlns='http://www.w3.org/2000/svg'xmlns:xlink='http://www.w3.org/1999/xlink'viewBox='0 24 150 28'preserveAspectRatio='none'><defs><path id='gentle-wave'd='M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z'></path></defs><g class='parallax'><use xlink:href='#gentle-wave'x='50'y='0'fill='rgba(128,215,175,.5)'></use><use xlink:href='#gentle-wave'x='50'y='3'fill='rgba(128,215,175,.5)'></use><use xlink:href='#gentle-wave'x='50'y='6'fill='rgba(128,215,175,.5)'></use></g></svg>"
+
+    //未完成
+    //var donghua2=document.createElement("footer");
+    //donghua2.className="svg-box2"
+    //donghua2.id="svgbox2"
+    //document.getElementsByTagName('body')[0].appendChild(donghua2)
+    //document.getElementsByClassName("svg-box2")[0].innerHTML="<svg class='editorial'xmlns='http://www.w3.org/2000/svg'xmlns:xlink='http://www.w3.org/1999/xlink'viewBox='0 24 150 28'preserveAspectRatio='none'><defs><path id='gentle-wave'd='M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z'></path></defs><g class='parallax'><use xlink:href='#gentle-wave'x='50'y='0'fill='rgba(128,215,175,.5)'></use><use xlink:href='#gentle-wave'x='50'y='3'fill='rgba(128,215,175,.5)'></use><use xlink:href='#gentle-wave'x='50'y='6'fill='rgba(128,215,175,.5)'></use></g></svg>"
+    //document.getElementsByClassName("svg-box2")[0].style.transform="rotate(180deg)"
+
 }
 
 function ClickBtn() {
